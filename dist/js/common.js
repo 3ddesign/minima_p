a3987748519feee25e78a28926a1d9a9579d3950
Dmitry Scherbakov <3ddesign@bk.ru>
2017-04-28 11:12:45 +0300

    all sections, mnu

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,3 +1,25 @@
 $(function () {
+  $("#services").click(function() {
+    $("html, body").animate({ scrollTop: $("header").height() }, "slow");
+    return false;
+  });
+  $("#story").click(function() {
+    $("html, body").animate({ scrollTop: $(".stor-sec").offset().top }, "slow");
+    return false;
+  });
+  $("#work").click(function() {
+    $("html, body").animate({ scrollTop: $(".rece-sec").offset().top }, "slow");
+    return false;
+  });
+  $("#journal").click(function() {
+    $("html, body").animate({ scrollTop: $(".sub-sec").offset().top }, "slow");
+    return false;
+  });
+  $("#contact").click(function() {
+    $("html, body").animate({ scrollTop: $(".soc-sec").offset().top }, "slow");
+    return false;
+  });
+
+$(".serv-item").animated("fadeInRight");
 
 });
